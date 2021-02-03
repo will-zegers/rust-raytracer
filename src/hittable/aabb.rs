41157c7cc804c1ceb2abeebd4650a79de8a03043
Will Zegers <will.zegers@gmail.com>
2021-02-03 01:48:23 +0000

    Fix AABB implementation

--- src/hittable/aabb.rs
+++ src/hittable/aabb.rs
@@ -10,25 +10,44 @@ pub struct AABB {
 }
 
 impl AABB {
-    pub fn hit(&self, r: &Ray, mut t_min: f64, mut t_max: f64) -> bool {
-        for i in 0..3 {
-            let t0 = f64::min(
-                (self.minimum[i] - r.origin[i]) / r.direction[i],
-                (self.maximum[i] - r.origin[i]) / r.direction[i],
-            );
-            let t1 = f64::max(
-                (self.minimum[i] - r.origin[i]) / r.direction[i],
-                (self.maximum[i] - r.origin[i]) / r.direction[i],
-            );
+    // slow
+    // pub fn hit(&self, r: &Ray, mut t_min: f64, mut t_max: f64) -> bool {
+    //     for i in 0..3 {
+    //         let t0 = f64::min(
+    //             (self.minimum[i] - r.origin[i]) / r.direction[i],
+    //             (self.maximum[i] - r.origin[i]) / r.direction[i],
+    //         );
+    //         let t1 = f64::max(
+    //             (self.minimum[i] - r.origin[i]) / r.direction[i],
+    //             (self.maximum[i] - r.origin[i]) / r.direction[i],
+    //         );
 
-            t_min = f64::max(t0, t_min);
-            t_max = f64::max(t1, t_max);
+    //         t_min = f64::max(t0, t_min);
+    //         t_max = f64::min(t1, t_max);
+    //         if t_max <= t_min {
+    //             return false;
+    //         }
+    //     }
+
+    //     true
+    // }
+    //
+    // optimized (?)
+    pub fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> bool {
+        for i in 0..3 {
+            let inv_d = 1. / r.direction[i];
+            let mut t0 = (self.minimum[i] - r.origin[i]) * inv_d;
+            let mut t1 = (self.maximum[i] - r.origin[i]) * inv_d;
+            if inv_d < 0. {
+                std::mem::swap(&mut t0, &mut t1);
+            }
+            let t_min = f64::max(t0, t_min);
+            let t_max = f64::min(t1, t_max);
 
             if t_max <= t_min {
                 return false;
             }
         }
-
         true
     }
 
@@ -44,6 +63,6 @@ impl AABB {
             f64::max(box0.maximum.z, box1.maximum.z),
         );
 
-        AABB { minimum, maximum }
+        Self { minimum, maximum }
     }
 }
